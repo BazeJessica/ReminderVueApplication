4c0263e8e616e0fa68cd1effbbc5ea4f5f47f769
Jessica <jessicaennor@gmail.com>
2024-06-02 14:59:29 +0200

    Created a task reminder application in vuejs

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="onClick()" :style="{background:color}" class="btn">
+        {{text}}
+        
+        </button>
+</template>
+
+
+<script>
+export default {
+    name: "ButtonComponent",
+
+    props: {
+        text :String,
+        color : String
+
+    },
+    
+    methods :{
+        onClick() {
+            console.log('clicked')
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :key v-for="task in tasks">
+      <h3>{{ task.text}} </h3>  
+        
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name:"TasksComponent",
+    props:{
+        tasks:Array
+    }
+}
+</script>
